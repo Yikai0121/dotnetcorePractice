d3d542c752af420b9c9dd9b60687adb5a61c81d7
Yikai0121 <k2lairrol@gmail.com>
2020-07-23 17:29:11 +0800

    創建項目上、中07230529

---
+++ 創建項目/創建項目/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using 創建項目.Services;
+
+namespace 創建項目.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IClock clock)
+        {
+
+        }
+    }
+}
